f33b7c1f7a628f9be36821de792048bf49212c71
Giga487 <Giga487@hotmail.com>
2020-07-14 18:30:45 +0200

    Ok PD

--- Tavola1BIcchi/initPD.m
+++ Tavola1BIcchi/initPD.m
@@ -7,30 +7,35 @@ addpath('utils');
 
 
 
-parameter = [1,1;
-             1,1;
+parameter = [0.50,1;
+             0.3,1;
              0,1;
-             1,1;
-             1,1;
-             1,1;];
+             0,1;
+             0,1;
+             0.4,1;];
          
 theta0 = [(-pi/2);(pi/2);0;(-pi/2);(pi/2);0];
 parameter = [parameter,theta0];
 
 % parameter = [d,m,theta0];
 
+q0 = [0,0,0,0,0,0]';
+dq = q0;
+[Pcom, Etip] = forwardKinematics(parameter, q0 );
 
-[Pcom, Etip] = forwardKinematics(parameter, [0,0,0,0,0,0]');
-
-x_com0 = Pcom;
+x_COM0 = Pcom;
 x_endEff0 = Etip(1:3,4);
 
-Kp = 100;
-Kd = 10;
+Kd_endEff = 5;
+K_endEff = [1000, Kd_endEff];
 
-[Pcom, Etip] = forwardKinematics(parameter, [pi/3,0,2,0,pi/3,0]');
+Kd_COM = 5;
+K_COM = [50, Kd_COM];
 
-x_com_D = Pcom;
+qf =  [pi/3,0,1,0,pi/3,0]';
+[Pcom, Etip] = forwardKinematics(parameter,qf);
+
+x_COM_D = [0;0;2];
 x_endEff_D = Etip(1:3,4);
 
 
