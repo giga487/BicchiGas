b066ee26c66906a6da83b8f96eb79845c9a22814
Giga487 <Giga487@hotmail.com>
2020-07-27 16:32:28 +0200

    PD e COMP

--- Tavola1BIcchi/initCompTorque.m
+++ Tavola1BIcchi/initCompTorque.m
@@ -10,9 +10,9 @@ addpath('utils/traj');
 parameter = [0.50,10;
              1,5;
              0,10;
-             0,2;
+             1,2;
              0,4;
-             0.4,5;];
+             1,5;];
          
 theta0 = [(-pi/2);(pi/2);0;(-pi/2);(pi/2);0];
 parameter = [parameter,theta0];
@@ -26,16 +26,15 @@ dq0 = q0;
 x_COM0 = Pcom;
 x_endEff0 = Etip(1:3,4);
 
-Kd_endEff = 1000;
-K_endEff = [1000, Kd_endEff];
+Kd_endEff = 1;
+K_endEff = [100, Kd_endEff];
 
 Kd_COM = 5;
 K_COM = [50, Kd_COM];
 
-qf =  [pi/3,pi/6,1.5,pi/2,pi/3,pi/4]';
+qf =  [-pi/3,pi/6,1.5,pi/2,pi/3,-pi/4]';
 [Pcom, Etip] = forwardKinematics(parameter,qf);
 
-x_COM_D = [0;0;2];
 x_endEff_D = Etip(1:3,4);
 
 %% generazione traiettorie
@@ -74,6 +73,7 @@ Switch_PD_OBJ = -1; % -1 è posizione fissa, 1 è insegumento traiettoria, switch 
 %% 
 sim('CompTorque',Simulation_Time);
 
-%% 
+%% PLOT
 
+plot_error(out, 'Computed Torque');
 
---
+++ Tavola1BIcchi/utils/plot_error.m
@@ -0,0 +1,61 @@
+
+function plot_error(data_sim, sim_string)
+
+    t_sim = data_sim.t_sim_CompTorque.signals.values;
+%     q_sim = data_sim.q_CompTorque.signals.values;
+    
+    [r,c] = size(data_sim.tau_CompTorque.signals.values);
+    
+    %% Tau
+    title_string = sprintf("%s: Tau", sim_string );
+    figure
+    for i = 1:c        
+
+        plot(t_sim,data_sim.tau_CompTorque.signals.values(:,i));
+        leg{i} = sprintf('Tau_%d', i);
+        hold on;
+
+    end
+    legend(leg);    
+    ylabel('tau [Nm]');
+    xlabel('time [s]');
+    title(title_string);
+    hold off;
+    grid on;
+
+    %% Error q
+    title_string = sprintf("%s: Error q", sim_string );
+    figure
+    for i = 1:c        
+
+        plot(t_sim,data_sim.e_joint_CompTorque.signals.values(:,i));
+        leg{i} = sprintf('e_%d', i);
+        hold on;
+
+    end
+    legend(leg);
+    ylabel('e [rad]');
+    xlabel('time [s]');
+    title(title_string);
+    hold off;
+    grid on;
+    
+    %% Error x
+    title_string = sprintf("%s: Error x", sim_string );
+    figure
+    for i = 1:3 %x y z        
+
+        plot(t_sim,data_sim.e_x_CompTorque.signals.values(:,i));
+        leg{i} = sprintf('e_x_%d', i);
+        hold on;
+
+    end
+    ylabel('e operative space [m]');
+    xlabel('time [s]');
+    legend(leg);
+    title(title_string);
+    hold off;
+    grid on;
+   
+
+end
